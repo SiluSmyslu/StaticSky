187eeaa13640b3fb95f81080a5f4b8035b4076f0
SiluSmyslu <135709234+SiluSmyslu@users.noreply.github.com>
2023-06-07 01:54:48 +0300

    Add files via upload

---
+++ krutilka1/data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cube",
+      "name": "cube",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.003392017097585409,
+        "pitch": 0.03462422281369193,
+        "fov": 1.607435465970391
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
